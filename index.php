7cb1953969d3f15808c21ec397a5861e65339ec0
FRANCESCO <OLIVOTTI>
2021-01-28 15:19:01 +0100

    trying to understand  get method

--- index.php
+++ index.php
@@ -3,20 +3,4 @@ $name = $argv[1];
 $surname = $argv[2];
 echo "ciao " . $name . " " . $surname;
 
-<h1>  $_GET["name"] </h1>
-?>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>php</title>
-</head>
-<body>
-
-
-
-    
-</body>
-</html>
