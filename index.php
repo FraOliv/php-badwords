bad8ec2c50df6aae25caa91c63daf4f81d408a09
FRANCESCO <OLIVOTTI>
2021-01-28 15:02:26 +0100

    testing

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+$name = $argv[1];
+$surname = $argv[2];
+echo "ciao " . $name . " " . $surname;
+
+<h1>  $_GET["name"] </h1>
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>php</title>
+</head>
+<body>
+
+
+
+    
+</body>
+</html>
